20828543e3e7e24a7c0ec55988c902486040e1ec
DariusDragnea <104531910+DariusDragnea@users.noreply.github.com>
2026-03-05 15:18:37 -0800

    RAM

---
+++ code/ip/ram1_port_64x19_inst.v
@@ -0,0 +1,7 @@
+ram1_port_64x19	ram1_port_64x19_inst (
+	.address ( address_sig ),
+	.clock ( clock_sig ),
+	.data ( data_sig ),
+	.wren ( wren_sig ),
+	.q ( q_sig )
+	);
---
+++ code/ip/ram1_port_64x8_inst.v
@@ -0,0 +1,7 @@
+ram1_port_64x8	ram1_port_64x8_inst (
+	.address ( address_sig ),
+	.clock ( clock_sig ),
+	.data ( data_sig ),
+	.wren ( wren_sig ),
+	.q ( q_sig )
+	);
